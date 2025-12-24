3a98b51fae82d09a3dee4d1012ca5942cd537f0e
Said Mariano <smariano170@gmail.com>
2025-12-24 12:57:46 -0600

    feat: Creación de apartados

--- src/ui/screens/home_screen.py
+++ src/ui/screens/home_screen.py
@@ -5,23 +5,71 @@ class DashboarPrincial(ft.Container):
     def __init__(self):
         super().__init__()
         
+        # self.page.adaptive = True
+
         #Contenidor 1
         self.texto_bienvenida = ft.Text(value='Hola, Said')
         self.configuracion = ft.Text(value='Icono de configuración')
 
-        self.content = ft.Row(
-            spacing=20,
-            controls=[
-                ft.Column(
-                spacing=20,
-                controls=[
-                    ft.Container(self.texto_bienvenida)
-                ]
-            ),
-            ft.Column(
-                spacing=20,
-                controls=[
-                    ft.Container(self.configuracion)
-                ]
-            )]
+        # Contenedor 3
+        self.dias_racha = ft.Text(value='0')
+        self.texto_dias_racha = ft.Text(value='Días Racha')
+
+        # Contenedor 4
+        self.fecha_actual = ft.Text(value='\nMié,24 Dic')
+
+        # Contenedor 5 
+        self.numero_tareas_pendientes = ft.Text(value='0')
+        self.texto_tareas_pendientes = ft.Text(value='Tareas Pendientes')
+
+        self.content = ft.SafeArea(
+            ft.Column([
+                ft.Row(
+                    spacing=20,
+                    controls=[
+                        ft.Column(
+                            spacing=20,
+                            controls=[
+                                ft.Container(self.texto_bienvenida)
+                            ]
+                        ),
+                        ft.Column(
+                            spacing=20,
+                            controls=[
+                                ft.Container(self.configuracion)
+                            ]
+                        )
+                    ]
+                ),
+                ft.Row(
+                    spacing=20,
+                    controls=[
+                        ft.Column(
+                            spacing=0,
+                            controls=[
+                                ft.Container(self.dias_racha),
+                                ft.Container(self.texto_dias_racha)
+                            ]
+                        ),
+                        ft.Column(
+                            spacing=20,
+                            controls=[
+                                ft.Container(self.fecha_actual)
+                            ]
+                        )
+                    ]
+                ),
+                ft.Row(
+                    spacing=20,
+                    controls=[
+                        ft.Column(
+                            spacing=0,
+                            controls=[
+                                ft.Container(self.numero_tareas_pendientes),
+                                ft.Container(self.texto_tareas_pendientes)
+                            ]
+                        )
+                    ]
+                )
+            ])
         )
