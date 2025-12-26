a0372ef0797b6623d4e263bc05f4030d278d4fa2
Said Mariano <smariano170@gmail.com>
2025-12-26 11:31:59 -0600

    feat: Se agrego la barra de navegación

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 import flet as ft
 from ui.screens.home_screen import DashboarPrincial
+from ui.components.nav_bar import BarraNavegacion
 
 def main(page: ft.Page):
-    page.scroll = True
+    # page.scroll = True
+
+    nav = BarraNavegacion(page)
     app = DashboarPrincial()
 
+    page.navigation_bar = nav
     page.add(app)
 
 
