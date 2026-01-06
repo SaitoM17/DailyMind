0e8ee86c20abe05abdd18d045acce60023a80762
Said Mariano <smariano170@gmail.com>
2026-01-06 13:35:31 -0600

    refactor: Nueva forma de crear las páginas

--- src/ui/screens/task_screen.py
+++ src/ui/screens/task_screen.py
@@ -1,18 +1,36 @@
 import flet as ft
-from ui.components.nav_bar import BarraNavegacion
+# from ui.components.nav_bar import BarraNavegacion
 
-class TareasScreen(ft.View):
+# class TareasScreen(ft.View):
+#     def __init__(self, page: ft.Page):
+#         super().__init__(
+#             route="/Tareas",
+#             navigation_bar=BarraNavegacion(page),
+#             padding=20,
+#             bgcolor="#F5F5F5",
+#             # ¡FALTABA ESTO! Sin controles, la vista es inválida
+#             controls=[
+#                 ft.AppBar(title=ft.Text("Mis Tareas"), bgcolor=ft.Colors.RED),
+#                 ft.Text("Aquí aparecerán tus tareas", size=20)
+#             ]
+#         )
+#         self.page = page
+
+class TareasScreen(ft.Container):
     def __init__(self, page: ft.Page):
-        super().__init__(
-            route="/Tareas",
-            navigation_bar=BarraNavegacion(page),
-            padding=20,
-            bgcolor="#F5F5F5",
-            # ¡FALTABA ESTO! Sin controles, la vista es inválida
-            controls=[
-                ft.AppBar(title=ft.Text("Mis Tareas"), bgcolor=ft.Colors.RED),
-                ft.Text("Aquí aparecerán tus tareas", size=20)
-            ]
-        )
-        self.page = page
+        super().__init__()
 
+
+        self.content = ft.SafeArea(
+            content=ft.Stack(
+                controls=[
+                    ft.Column(
+                        scroll=ft.ScrollMode.ADAPTIVE,
+                        expand=True,
+                        controls=[
+                            ft.Text('Texto de prueba de la nueva versión de la página de tareas')
+                        ]
+                    )
+                ]
+            )
+        )
