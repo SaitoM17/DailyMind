4f78c4e16b448ee042b38879a67af99170356ef0
Said Mariano <smariano170@gmail.com>
2026-01-07 13:10:30 -0600

    feat: Se agrego AppBar con función

--- src/ui/screens/create_edit_task.py
+++ src/ui/screens/create_edit_task.py
@@ -1,6 +1,18 @@
 import flet as ft
 
 def CrearEditarEliminarTareaScreen(page: ft.Page):
+    
+    appbar = ft.AppBar(
+        leading=ft.IconButton(
+            ft.Icons.CLEAR,
+            icon_size=25, 
+            on_click=lambda _:page.go('/')
+            ),
+            title=ft.Text('Crear Tarea'),
+            center_title=True,
+            bgcolor=ft.Colors.with_opacity(0.04, ft.CupertinoColors.SYSTEM_BACKGROUND),
+    )
+
     # Campos de texto para la tarea
     task_input = ft.TextField(label="¿Qué hay que hacer?", autofocus=True)
     
@@ -11,7 +23,7 @@ def CrearEditarEliminarTareaScreen(page: ft.Page):
     return ft.View(
         "/create",
         controls=[
-            ft.AppBar(title=ft.Text("Crear Nueva Tarea"), bgcolor=ft.Colors.AMBER),
+            appbar,
             ft.SafeArea(
                 content=ft.Column([
                     ft.Text("Detalles de la tarea", size=20, weight="bold"),
