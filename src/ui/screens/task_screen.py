a7618d754f4b4da3a49410ca10344256feddabf5
Said Mariano <smariano170@gmail.com>
2026-01-08 17:04:59 -0600

    feat: Se agrego boton junto con contenedor

--- src/ui/screens/task_screen.py
+++ src/ui/screens/task_screen.py
@@ -1,24 +1,18 @@
 import flet as ft
-# from ui.components.nav_bar import BarraNavegacion
-
-# class TareasScreen(ft.View):
-#     def __init__(self, page: ft.Page):
-#         super().__init__(
-#             route="/Tareas",
-#             navigation_bar=BarraNavegacion(page),
-#             padding=20,
-#             bgcolor="#F5F5F5",
-#             # ¡FALTABA ESTO! Sin controles, la vista es inválida
-#             controls=[
-#                 ft.AppBar(title=ft.Text("Mis Tareas"), bgcolor=ft.Colors.RED),
-#                 ft.Text("Aquí aparecerán tus tareas", size=20)
-#             ]
-#         )
-#         self.page = page
+from ui.components.button_task import ButtonCrearTarea
 
 class TareasScreen(ft.Container):
     def __init__(self, page: ft.Page):
         super().__init__()
+
+        self.boton_flotante = ft.Column(
+            controls=[
+                ButtonCrearTarea(page)
+            ],
+            alignment=ft.MainAxisAlignment.END,
+            horizontal_alignment=ft.CrossAxisAlignment.END,
+            spacing=10
+        )
 
 
         self.content = ft.SafeArea(
@@ -30,6 +24,11 @@ class TareasScreen(ft.Container):
                         controls=[
                             ft.Text('Texto de prueba de la nueva versión de la página de tareas')
                         ]
+                    ),
+                    ft.Container(
+                        content=self.boton_flotante,
+                        bottom=10,
+                        right=10
                     )
                 ]
             )
