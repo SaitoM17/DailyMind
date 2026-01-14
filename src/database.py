d9794ad42a1d010d1efe78253fb423253bb227af
Said Mariano <smariano170@gmail.com>
2026-01-14 14:08:47 -0600

    feat: Se agregaron funciones set y get junto con tados de prueba

--- src/database.py
+++ src/database.py
@@ -1,1 +1,20 @@
-tareas_globales = []  # Aquí se guardarán los diccionarios de cada tarea
+tareas_globales = []
+
+nueva_tarea = {
+        "nombre": 'Comprar',
+        "fecha": '23-jun-26',
+        "prioridad": 'Alta',
+        "completada": False
+    }
+
+tareas_globales.append(nueva_tarea)
+
+def get(key):
+    if key == "mis_tareas":
+        return tareas_globales
+    return None
+
+def set(key, value):
+    if key == "mis_tareas":
+        tareas_globales.clear()
+        tareas_globales.extend(list(value))
