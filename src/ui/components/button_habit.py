26fa3a9c257fddb9b265e9f8f284491685e7cf77
Said Mariano <smariano170@gmail.com>
2026-01-17 15:30:49 -0600

    feat: Se agrego fucnión para recordadr última ventana

--- src/ui/components/button_habit.py
+++ src/ui/components/button_habit.py
@@ -3,6 +3,8 @@ import flet as ft
 class ButtonCrearHabito(ft.ElevatedButton):
     def __init__(self, page: ft.Page):
         super().__init__()
+        self.page = page
+
         self.content=ft.Row(
             [
                 ft.Text(value='Hábito', size=18),
@@ -11,12 +13,13 @@ class ButtonCrearHabito(ft.ElevatedButton):
             tight=True,
             spacing=10
         )
-        self.on_click = self.nueva_tarea
+        self.on_click = self.nuevo_habito
         self.style=ft.ButtonStyle(
                 color=ft.Colors.BLACK,
                 bgcolor="#ffffff",
                 shape=ft.RoundedRectangleBorder(radius=15),
             )
 
-    def nueva_tarea(self, e):
+    def nuevo_habito(self, e):
+        self.page.client_storage.set('return_route', self.page.route)
         self.page.go("/CreateHabito")
