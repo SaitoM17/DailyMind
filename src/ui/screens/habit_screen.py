06226100827af4eb3cfca0f41921112ac631aae6
Said Mariano <smariano170@gmail.com>
2026-01-17 14:01:11 -0600

    feat: Creación de contenedor 2 racha

--- src/ui/screens/habit_screen.py
+++ src/ui/screens/habit_screen.py
@@ -41,6 +41,40 @@ class HabitosScreen(ft.Container):
             border_radius=20
         )
 
+        # Contenedor 2 Racha
+
+        self.icono_contenedor2 = ft.Icon(name=ft.Icons.LOCAL_FIRE_DEPARTMENT, color=ft.Colors.RED, weight=ft.FontWeight.BOLD)
+        self.texto_racha = ft.Text(value='RACHA', weight=ft.FontWeight.BOLD, color=ft.Colors.BLACK)
+        self.sub_contenedor2 = ft.Row(
+            controls=[
+                ft.Column(controls=[self.icono_contenedor2], 
+                          alignment=ft.MainAxisAlignment.CENTER, 
+                          horizontal_alignment=ft.CrossAxisAlignment.CENTER),
+                ft.Column(controls=[self.texto_racha], 
+                          alignment=ft.MainAxisAlignment.CENTER, 
+                          horizontal_alignment=ft.CrossAxisAlignment.CENTER)
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER
+        )
+        self.dias_racha = ft.Text(value='12', weight=ft.FontWeight.BOLD, color=ft.Colors.BLACK)
+        self.texto_contenedor2 = ft.Text(value='Días seguidos', weight=ft.FontWeight.BOLD, color=ft.Colors.BLACK)
+
+        self.contenedor_racha = ft.Container(
+            content=ft.Column(
+                controls=[
+                    self.sub_contenedor2,
+                    self.dias_racha,
+                    self.texto_contenedor2
+                ],
+                alignment=ft.alignment.center,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER
+            ),
+            alignment=ft.alignment.center,
+            bgcolor=ft.Colors.WHITE,
+            border_radius=20
+        )
+
         self.content = ft.SafeArea(
             content=ft.Stack(
                 controls=[
@@ -49,7 +83,8 @@ class HabitosScreen(ft.Container):
                         expand=True,
                         controls=[
                             texto_prueba,
-                            self.contenedor_habitos
+                            self.contenedor_habitos,
+                            self.contenedor_racha
                         ]
                     )
                 ]
