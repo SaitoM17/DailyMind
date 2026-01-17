83105a05d07046a2d60e98b58e154359d3c7fc59
Said Mariano <smariano170@gmail.com>
2026-01-17 13:51:21 -0600

    feat: Creación de contenedor 1 de hábitos pendientes

--- src/ui/screens/habit_screen.py
+++ src/ui/screens/habit_screen.py
@@ -6,6 +6,41 @@ class HabitosScreen(ft.Container):
 
         texto_prueba = ft.Text('Texto de prueba, pantalla Hábitos')
 
+        # Contenedor 1 Habitos Pendientes
+        self.icono_contenedor1 = ft.Icon(name=ft.Icons.VERIFIED, color=ft.Colors.YELLOW, weight=ft.FontWeight.BOLD)
+        self.fecha_contenedor1 = ft.Text(value='Hoy', weight=ft.FontWeight.BOLD, color=ft.Colors.BLACK)
+        self.sub_contenedor1 = ft.Row(
+            controls=[
+                ft.Column(controls=[self.icono_contenedor1], 
+                          alignment=ft.MainAxisAlignment.CENTER, 
+                          horizontal_alignment=ft.CrossAxisAlignment.CENTER),
+                ft.Column(controls=[self.fecha_contenedor1], 
+                          alignment=ft.MainAxisAlignment.CENTER, 
+                          horizontal_alignment=ft.CrossAxisAlignment.CENTER)
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER
+        )
+        self.numerador_habitos = int(1)
+        self.denominador_habitos = int(2)
+        self.cantidad_habitos = ft.Text(value=f'{self.numerador_habitos}/{self.denominador_habitos}', weight=ft.FontWeight.BOLD, color=ft.Colors.BLACK)
+        self.texto_contenedor1 = ft.Text(value='Hábitos completados', weight=ft.FontWeight.BOLD, color=ft.Colors.BLACK)
+
+        self.contenedor_habitos = ft.Container(
+            content=ft.Column(
+                controls=[
+                    self.sub_contenedor1,
+                    self.cantidad_habitos,
+                    self.texto_contenedor1
+                ],
+                alignment=ft.alignment.center,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER
+            ),
+            alignment=ft.alignment.center,
+            bgcolor=ft.Colors.WHITE,
+            border_radius=20
+        )
+
         self.content = ft.SafeArea(
             content=ft.Stack(
                 controls=[
@@ -13,7 +48,8 @@ class HabitosScreen(ft.Container):
                         scroll=ft.ScrollMode.ADAPTIVE,
                         expand=True,
                         controls=[
-                            texto_prueba
+                            texto_prueba,
+                            self.contenedor_habitos
                         ]
                     )
                 ]
