31b7979c5e920fdc8c30876bb7458f16e4056edc
Said Mariano <smariano170@gmail.com>
2026-01-17 17:04:59 -0600

    feat: Se agrego medición de hábitos

--- src/ui/screens/create_edit_habit.py
+++ src/ui/screens/create_edit_habit.py
@@ -31,7 +31,27 @@ class CrearEditarEliminarHabitoScreen(ft.Container):
         self.nombre_habito = ft.Text(value='Nombre del Hábito')
         self.habito_input = ft.TextField(label='Ej. Leer 10 páginas', autofocus=True)
 
-        # Definimos el contenido siguiendo tu estructura
+        # Medición
+        self.texto_medicion = ft.Text(value='¿Cómo quieres medirlo?')        
+        self.contenedor_medicion = ft.RadioGroup(
+            content=ft.Row(
+                [
+                    ft.Radio(
+                        value="1",
+                        label="Completar (sí/no)",
+                        adaptive= True,
+                        active_color=ft.Colors.GREEN,
+                    ),
+                    ft.Radio(
+                        value="2",
+                        label="Medible",
+                        adaptive=True,
+                        active_color=ft.Colors.BLUE,
+                    ),
+                ]
+            )
+        )
+
         self.content = ft.SafeArea(
             content=ft.Stack(
                 controls=[
@@ -42,7 +62,9 @@ class CrearEditarEliminarHabitoScreen(ft.Container):
                         controls=[
                             self.appbar,                            
                             self.nombre_habito,
-                            self.habito_input
+                            self.habito_input,
+                            self.texto_medicion,
+                            self.contenedor_medicion
                         ]
                     )
                 ]
